56b53398054b71232e332a3bb9151fc98b3860a7
Joyann Boyce <joyannboyce30@gmail.com>
2023-02-23 14:45:51 +0000

    update flask
    Signed-off-by: Joyann Boyce <joyannboyce30@gmail.com>

--- tool.py
+++ tool.py
@@ -1,58 +1,60 @@
 from flask import Flask, render_template, request, jsonify
-from airtable import Airtable
+import csv
 
 #state this is the app
 app = Flask(__name__)
 
-# Your Airtable API key and base ID
-AIRTABLE_API_KEY = 'patwLoFtU6uCVQLbr.f21c5c8ee50b4462d3546257f605e31d0f81abab9420a7e15a9f1129f257b38e'
-AIRTABLE_BASE_ID = 'appS6ep3oZKd27r1U'
-AIRTABLE_TABLE_NAME = 'Dictionary'
+import csv
+
+data = []
 
 
-# Create an Airtable object to interact with your base
-airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=AIRTABLE_API_KEY)
+# Load the data from the CSV file
+with open('dictionary.csv', 'r') as infile:
+    reader = csv.DictReader(infile)
+    for row in reader:
+        data.append(row)
 
-
-
-# Your homepage route
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-# Your form submission route
-@app.route('/check', methods=['POST'])
+@app.route('/', methods=['POST'])
 def check_text():
-    # Get the user's input text from the form
+    # Get the user input from the form
     text = request.form['text']
 
-    # Split the text into a list of words
+    # Split the text into words
     words = text.split()
 
-    # Look up each word in your Airtable base and check if it's offensive
+    # Initialize a list to store any offensive words
     offensive_words = []
+
+    # Loop through each word in the text
     for word in words:
-        if airtable.search('Word', word):
-            offensive_words.append(word)
+        # Check if the word is in the dictionary
+        for item in data:
+            if word.lower() == item['Word'].lower():
+                # If the word is in the dictionary, add it to the list of offensive words
+                offensive_words.append(word)
 
-    # If no offensive words are found, return a success message
-    if not offensive_words:
-        response = {
-            'status': 'success',
-            'message': 'Your text contains no offensive words!'
-        }
-    # If offensive words are found, return a list of suggestions
-    else:
-        suggestions = []
-        for word in offensive_words:
-            record = airtable.search('Word', word)[0]
-            suggestions.append(record['fields'].get('Alt word', ''))
-        response = {
-            'status': 'warning',
-            'message': 'Your text contains offensive words. Here are some suggestions:',
-            'offensive_words': offensive_words,
-            'suggestions': suggestions
-        }
+    # Initialize a list to store the suggested replacements
+    replacements = []
 
-    # Return the response as a JSON object
-    return jsonify(response)
+    # Loop through each offensive word
+    for word in offensive_words:
+        # Find the corresponding entry in the dictionary
+        for item in data:
+            if word.lower() == item['Word'].lower():
+                # If an alternative word is available, add it to the list of replacements
+                if item['Alt word']:
+                    replacements.append(item['Alt word'])
+                # Otherwise, use the original word as the replacement
+                else:
+                    replacements.append(word)
+
+    # Render the results page with the offensive words and suggested replacements
+    return render_template('results.html', text=text, offensive_words=offensive_words, replacements=replacements)
+
+if __name__ == '__main__':
+    app.run(debug=True)
